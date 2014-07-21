d9a79c3109fcd7d1ea07773678b10914a76401c0
Brian Mathiyakom <brian@rarevisions.net>
2014-07-20 20:07:22 -0700

    Some more documentation.

--- lib/claws.rb
+++ lib/claws.rb
@@ -1,2 +1,7 @@
 require "claws/version"
 require "claws/thread_pool"
+
+# All code in the gem is namespaced under this module.
+module Claws
+
+end
--- lib/claws/version.rb
+++ lib/claws/version.rb
@@ -1,3 +1,4 @@
 module Claws
+  # The current version of claws
   VERSION = "0.0.1"
 end
